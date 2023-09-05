b6638320d8b4e71e64dc3a643e64bb8e840d0619
ayoubim <ayoubim@berea.edu>
2023-09-05 15:02:35 -0400

    Completed Readme

---
+++ test_suite.py
@@ -0,0 +1,64 @@
+#PV Calculator
+#This project involves the development of a PV (Present Value) calculator that facilitates calculations related to time value of money. The calculator can determine future and present values of money, compute the net present value of potential investments, annuity payments, and periodic payments.
+#
+#Milestones
+#Milestone 1: Setup, Planning, Design
+#Due Date: 7 Apr 2023
+#
+#Milestone 2: Code
+#Due Date: 14 Apr 2023
+#Status: Completed
+#
+#Milestone 3: Virtual Check-In
+#Due Date: 21 Apr 2023
+#Completion: 70%
+#Confidence: 95%
+#
+#Milestone 4: Final Code, Presentation, Demo
+#Section A Due: 4 May 2023
+#Section B Due: 3 May 2023
+#
+#User Instructions
+#This program is a Time Value of Money (TVM) calculator featuring a graphical user interface. Developed using Python and the Tkinter module, the interface provides a form with input fields for different TVM parameters such as present value, future value, interest rate, payment, and period. Users can select a parameter and input corresponding values. Upon clicking the "Calculate" button, the program employs various methods to perform TVM calculations. Notably, it rounds present value results to two decimal places. It's important to mention that the messagebox module used within the GUI environment for error handling may need replacement for non-GUI contexts. Additionally, the program does not account for compounding frequency and assumes end-of-period payments.
+#
+#Reflection
+#Project Choice
+#Our decision to develop a time value of money calculator stemmed from a passion for financial modeling and algorithms. Leveraging our finance background, we found this project a practical way to apply our skills. We view it as a stepping stone toward our ambitions in the financial modeling realm.
+#
+#Project Evolution
+#Comparing our final project with the initial design, we'd rate the alignment at 8 out of 10. While successful in implementing accurate calculations, we acknowledge room for improvement in terms of compounding frequency inclusion and design aesthetics.
+#
+#Learning Experience
+#This project enriched our understanding of time value of money, demanding precise comprehension to implement it effectively. It also deepened our familiarity with the Tkinter library and underscored the significance of rigorous testing and documentation.
+#
+#Challenges
+#The most challenging aspect was establishing the linkage between radio buttons, entries, and the calculation function. Overcoming this required focused effort and problem-solving.
+#
+#Future Improvements
+#Given our present insights, our approach for a similar project would differ. We'd allocate more time to meticulous planning, ensuring comprehensive comprehension of project objectives. Additionally, we'd enhance code comments for clarity and future adaptability.
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
